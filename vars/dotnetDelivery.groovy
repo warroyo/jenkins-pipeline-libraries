74aa101453fa1ec6533f13fad75efeed8d1a8597
warroyo <warroyo7199008@gmail.com>
2019-01-28 17:22:14 -0700

    no need for nested stages

--- vars/dotnetDelivery.groovy
+++ vars/dotnetDelivery.groovy
@@ -11,65 +11,64 @@ def call(Closure body) {
             APP_NAME = "${pipelineParams.appName}"
         }
         stages {
-            stage('CI') {
-                stages {
-                    stage('Build') {
-                        environment {
-                            HOME = '/tmp'
-                        } 
-                        agent {
-                            docker { image 'microsoft/dotnet:2.2-sdk' }
-                        }
-                        steps {
-                            echo 'Building..'
-                            sh script:'dotnet build --configuration Release', label: 'build app'
-                            sh script: 'dotnet publish --configuration Release --output artifact', label: 'publish artifact'
-                            sh script: 'cp *.yml artifact/.', label: 'copy manifests to artifact'
-                            archiveArtifacts artifacts: 'artifact/*'
-                        }
-                    }
-                    stage('Test') {
-                        steps {
-                            echo 'Testing..'
-                            sh 'ls -l'
-                        }
-                    }
+
+            stage('Build') {
+                environment {
+                    HOME = '/tmp'
+                } 
+                agent {
+                    docker { image 'microsoft/dotnet:2.2-sdk' }
+                }
+                steps {
+                    echo 'Building..'
+                    sh script:'dotnet build --configuration Release', label: 'build app'
+                    sh script: 'dotnet publish --configuration Release --output artifact', label: 'publish artifact'
+                    sh script: 'cp *.yml artifact/.', label: 'copy manifests to artifact'
+                    archiveArtifacts artifacts: 'artifact/*'
                 }
             }
-            stage('Dev - CD'){
+            stage('Test') {
+                steps {
+                    echo 'Testing..'
+                    sh 'ls -l'
+                }
+            }
+
+
+            stage('Deploy Venerable - Dev') {
                 agent {
                     docker { image 'nulldriver/cf-cli-resource' }
                 }
-                stages{
-                    stage('Deploy Venerable') {
-                        options {
-                            skipDefaultCheckout true
-                        }
-                        steps {
-                            deployVenerable(pipelineParams.dev)
-                        }
-                    }
-                    stage('Smoke Test') {
-                        agent {
-                            docker { image 'postman/newman' }
-                        }
-                        options {
-                            skipDefaultCheckout true
-                        }
-                        steps {
-                            echo 'Running tests'
-                        }
-                    }
-                    stage('Flip') {
-                        options {
-                            skipDefaultCheckout true
-                        }
-                        steps {
-                           flip(pipelineParams.dev)
-                        }
-                    }
+                options {
+                    skipDefaultCheckout true
+                }
+                steps {
+                    deployVenerable(pipelineParams.dev)
                 }
             }
+            stage('Smoke Test') {
+                agent {
+                    docker { image 'postman/newman' }
+                }
+                options {
+                    skipDefaultCheckout true
+                }
+                steps {
+                    echo 'Running tests'
+                }
+            }
+            stage('Flip') {
+                agent {
+                    docker { image 'nulldriver/cf-cli-resource' }
+                }
+                options {
+                    skipDefaultCheckout true
+                }
+                steps {
+                    flip(pipelineParams.dev)
+                }
+            }
+
             // stage('QA - CD'){
             //     steps{
             //     cd(apiUrl: pipelineParams.qaApiUrl, org: pipelineParams.qaOrg, space: pipelineParams.qaSpace,
