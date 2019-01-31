aaecefa5027facbadfd8ac8a0426adb256a030b7
warroyo <warroyo7199008@gmail.com>
2019-01-31 00:33:24 -0700

    add  home dir

--- vars/smoke.groovy
+++ vars/smoke.groovy
@@ -3,9 +3,7 @@ def call(def cdParams) {
     contents = readYaml (file: "${cdParams.envFile}") 
     domain = contents.domain.toString()
     env.ROUTE = "https://${env.APP_NAME}.${domain}"
-    echo env.ROUTE
+    env.HOME='/tmp'
 
-    sh script: 'ls -l'
-    sh script: 'which inspec', label: 'running smoke tests'
     sh script: 'inspec exec smoke', label: 'running smoke tests'
 }
