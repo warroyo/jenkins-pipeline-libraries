a5bc77fe611266517111fc50164ebe36c13e26be
warroyo <warroyo7199008@gmail.com>
2019-01-30 22:47:44 -0700

    testing

--- vars/smoke.groovy
+++ vars/smoke.groovy
@@ -3,6 +3,7 @@ def call(def cdParams) {
     contents = readYaml (file: "${cdParams.envFile}") 
     domain = contents.domain.toString()
     env.ROUTE = "${env.APP_NAME}.${domain}"
+    echo env.ROUTE
 
     sh script: 'ls -l'
     sh script: 'which inspec', label: 'running smoke tests'
