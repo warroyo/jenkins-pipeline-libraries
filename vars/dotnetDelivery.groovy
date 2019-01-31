ef2160535450d6ff7c66979100d0fa58d5b5a779
warroyo <warroyo7199008@gmail.com>
2019-01-30 23:40:50 -0700

    override entry

--- vars/dotnetDelivery.groovy
+++ vars/dotnetDelivery.groovy
@@ -63,7 +63,7 @@ def call(Closure body) {
                 agent {
                     docker { 
                         image 'chef/inspec'
-                        args '-it --entrypoint=" "'
+                        args '-it --entrypoint="sh"'
                      }
                 }
                 options {
