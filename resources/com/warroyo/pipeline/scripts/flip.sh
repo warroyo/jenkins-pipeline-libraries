9eba665936a7147fe2826d6a4d5bf6439e343a7a
warroyo <warroyo7199008@gmail.com>
2019-01-30 20:57:01 -0700

    rename

--- resources/com/warroyo/pipeline/scripts/flip.sh
+++ resources/com/warroyo/pipeline/scripts/flip.sh
@@ -4,4 +4,4 @@ set -e
 cf map-route ${APP_NAME}-green $ROUTE --hostname ${APP_NAME}
 cf delete $APP_NAME -f
 cf rename ${APP_NAME}-green $APP_NAME
-cf delete-route $ROUTE --hostname ${APP_NAME}-venerable -f
+cf delete-route $ROUTE --hostname ${APP_NAME}-green -f
